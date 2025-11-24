4696d305c687819d4c24f8c1b3d0d6064d1a7a93
Egor <arnoldtagirov1@gmail.com>
2025-11-24 19:09:58 +0100

    show,movie was added

---
+++ GC/Domain/Entities/Film.cs
@@ -0,0 +1,30 @@
+﻿namespace GC.Domain.Entities
+{
+    public class Movie
+    {
+
+        public double Duration {  get; set; }
+        public int Year { get; set; }
+        public string Title {  get; set; }
+        public string Genre { get; set; }
+        public Movie(string title, string genre, int year,double duration) 
+        {
+            Title = title;
+            Genre = genre;
+            Year = year;
+            Duration = duration;
+
+        }
+        public void Play()
+        {
+            Console.WriteLine($"You are watching Movie {Title}");
+            Console.WriteLine($"Duration: {Duration}");
+        }
+        ~Movie()
+        {
+            Console.WriteLine("Movie was removed!");
+        }
+
+
+    }
+}
---
+++ GC/Domain/Entities/Show.cs
@@ -0,0 +1,30 @@
+﻿namespace GC.Domain.Entities
+{
+    public  class Show:IDisposable
+    {
+        public string Title {  get; set; }
+
+        public string Genre { get; set; }
+
+        public double Duration { get; set; }
+
+        public int CountOfActors {  get; set; }
+
+        public Show(string title,string genre,double duration,int countofacters)
+        {
+            Title = title;
+            Genre = genre;
+            Duration = duration;
+            CountOfActors = countofacters;
+
+        }
+        public void InfoAboutShow()
+        {
+            Console.WriteLine($"Name {Title}\nGenre {Genre}\nDuration {Duration}\nCount of Actors {CountOfActors}");
+        }
+       public void Dispose()
+        {
+            Console.WriteLine("Disposed");
+        }
+    }
+}
