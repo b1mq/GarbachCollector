0964f9f413c63d4d46ef82ac1d5a03e75a29c202
Egor <arnoldtagirov1@gmail.com>
2025-11-24 19:27:59 +0100

    finished

---
+++ GC/Domain/Entities/Play.cs
@@ -0,0 +1,31 @@
+﻿namespace GC.Domain.Entities
+{
+    public class Play:IDisposable // пьеса на английском 
+    {
+        public string Title { get; set; }
+
+        public string NameOfAutor {  get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public Play(string title, string genre,string nameofautor, int year)
+        {
+            Title = title;
+            Genre = genre;
+            NameOfAutor = nameofautor;
+            Year = year;
+        }
+        public override string ToString()
+        {
+            return $"Title: {Title}\nName of Autor: {NameOfAutor}\nGenre: {Genre}\nYear: {Year}";
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Play disposed manually!");
+        }
+
+        ~Play()
+        {
+            Console.WriteLine("By Destructor");
+        }
+    }
+}
---
+++ GC/Domain/Entities/Shop.cs
@@ -0,0 +1,32 @@
+﻿namespace GC.Domain.Entities
+{
+    public class Shop
+    {
+        public string NameOfShop { get; set; }
+        public string Adress { get; set; }
+        public string TypeOfShop { get; set; }
+
+        public Shop(string name, string adress, string type)
+        {
+            NameOfShop = name;
+            Adress = adress;
+            TypeOfShop = type;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Shop: {NameOfShop}");
+            Console.WriteLine($"Adress: {Adress}");
+            Console.WriteLine($"Type: {TypeOfShop}");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Shop disposed manually!");
+        }
+
+        ~Shop()
+        {
+            Console.WriteLine("Shop was removed by GC!");
+        }
+    }
+}
